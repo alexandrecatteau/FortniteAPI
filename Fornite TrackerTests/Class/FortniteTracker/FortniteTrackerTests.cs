18d4514372aa72f9e024cc0994a086dcd53d9995
Alex <Alex@DESKTOP-ALEX>
2018-04-21 21:38:56 +0200

    SUpression tests

---
+++ Fornite TrackerTests/Class/FortniteTracker/FortniteTrackerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fornite_Tracker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fornite_Tracker.Tests
+{
+    [TestClass()]
+    public class FortniteTrackerTests
+    {
+        [TestMethod()]
+        public void GetStatTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
